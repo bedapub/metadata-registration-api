4c13e8f88fa0c0c1d0bfb255250988b77cda322c
Bioinformatics Batch Account <rafael.mueller@roche.com>
2020-02-17 10:49:46 +0100

    Feat: Run API as Podman container
    > Create startup script

---
+++ start_podman.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#==============================================================================
+# Script Name: start_podman.sh.
+# Description: Build and run a podman container. If the container is already
+#              running, the running container is stopped and deleted prior.
+#      Author: Rafael Müller
+#       Email: rafael.mueller1@gmail.com
+#  Created on: 17 Feb, 2020
+#==============================================================================
+
+
+IMG_NAME="meta_registration_api"
+CONT_NAME="meta_registration_api_cont"
+PORT=5001
+
+if [ ! "$(sudo podman ps -q -f name='$CONT_NAME')" ]; then
+  echo "Found container with same name"
+
+  if [ ! "$(sudo podman ps -aq -f status=exited -f name='$CONT_NAME')" ]; then
+    echo "Container is still running. Stop container \"$CONT_NAME now\""
+    sudo podman stop "$CONT_NAME"
+  fi
+
+  echo "Remove container $CONT_NAME"
+  sudo podman rm "$CONT_NAME"
+
+fi
+
+echo "Build container"
+sudo podman build -t "$CONT_NAME" .
+echo "Run container"
+sudo podman run -d -p "$PORT":8000 --name "$CONT_NAME" "$IMG_NAME"
