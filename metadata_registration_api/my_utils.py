c1ba490c6279c33d8004ebf0a25ab2b9c82d69c0
Cyril Lopez <cyrlop06@hotmail.com>
2020-07-31 09:24:00 +0000

    Removed utils migrated to lib and related tests

--- metadata_registration_api/my_utils.py
+++ metadata_registration_api/my_utils.py
@@ -3,28 +3,3 @@ import requests
 
 def str_to_bool(s):
     return s.lower() in ["true", "1", "y", "yes", "on", "t"]
-
-
-def map_key_value(url, key="id", value="name"):
-    """Call API at url endpoint and create a dict which maps key to value
-
-    If the response contains identical keys, only the last value is stored for this key. The mapping only works
-    for fields in the top level (no nested fields).
-
-    :param url: API endpoint to call
-    :type url: str
-    :param key: The key by which the value will be found
-    :type key: str
-    :param value: The value to which the key will map
-    :type value: str
-    ...
-    :return: A dict with maps key -> value
-    :rtype: dict
-
-    """
-    res = requests.get(url, headers={"x-Fields": f"{key}, {value}"})
-
-    if res.status_code != 200:
-        raise Exception(f"Request to {url} failed with key: {key} and value: {value}. {res.json()}")
-
-    return {entry[key]: entry[value] for entry in res.json()}
