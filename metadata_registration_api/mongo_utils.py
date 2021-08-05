6b1ea35dd5f2e5f4eb110ae8d06e1d1e457ff3b7
Cyril Lopez <cyrlop06@hotmail.com>
2021-08-05 15:19:48 +0200

    Added separate way to get mongo client with proper authentification

--- metadata_registration_api/mongo_utils.py
+++ metadata_registration_api/mongo_utils.py
@@ -1,6 +1,28 @@
+from pymongo import MongoClient
+
+
+def get_client(app):
+    if app.config["MONGODB_USERNAME"] and app.config["MONGODB_PASSWORD"]:
+        return MongoClient(
+            host=app.config["MONGODB_HOST"],
+            port=app.config["MONGODB_PORT"],
+            username=app.config["MONGODB_USERNAME"],
+            password=app.config["MONGODB_PASSWORD"],
+            authSource=app.config["MONGODB_DB"],
+            authMechanism="SCRAM-SHA-1",
+        )
+    else:
+        return MongoClient(
+            host=app.config["MONGODB_HOST"],
+            port=app.config["MONGODB_PORT"],
+            authSource=app.config["MONGODB_DB"],
+        )
+
+
 def get_states(app, q={}):
     """Returns states as list of dict directly from MongoDB"""
-    db = app.mongo_client[app.config["MONGODB_DB"]]
+    client = get_client(app)
+    db = client[app.config["MONGODB_DB"]]
     cur = db[app.config["MONGODB_COL_STATE"]]
     results = cur.find(q)
 
